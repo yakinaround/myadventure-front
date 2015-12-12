ace7dbe78822e0c5b58c79436bcf70f4c02fda17
Martin Spier <spiermar@gmail.com>
2015-12-11 20:42:51 -0800

    Index page with adventure list.

--- app/mod_index/controllers.py
+++ app/mod_index/controllers.py
@@ -3,11 +3,17 @@ controllers.py
 
 Index module controllers.
 """
-from flask import Blueprint, abort
+import requests
+from flask import Blueprint, render_template
+
+from app.mod_api import _url
 
 mod_index = Blueprint('index', __name__, url_prefix='')
 
 
 @mod_index.route('/')
 def index():
-    return abort(404)
+    r = requests.get(_url('/adventure'))
+    print r.json()
+    adventures = r.json()
+    return render_template('index.html', adventures=adventures['adventures'])
