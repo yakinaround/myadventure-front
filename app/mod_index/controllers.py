cf012f90eac3eb913a18b8cdea1c64c3b0df772a
Martin Spier <spiermar@gmail.com>
2016-01-24 13:33:33 -0800

    Sticky footer. Splitting main.css into separate files.

--- app/mod_index/controllers.py
+++ app/mod_index/controllers.py
@@ -21,3 +21,8 @@ def recent():
     r = requests.get(_url('/adventure'))
     adventures = r.json()
     return render_template('recent.html', adventures=adventures['adventures'])
+
+
+@mod_index.route('/signup')
+def signup():
+    return render_template('signup.html')
