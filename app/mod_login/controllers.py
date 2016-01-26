a86fd6a2567b8ac03bf7e23bda7bd87f23fa8a4c
Martin Spier <spiermar@gmail.com>
2016-01-26 09:06:16 -0800

    [WIP] Working login form.

--- app/mod_login/controllers.py
+++ app/mod_login/controllers.py
@@ -4,53 +4,48 @@ controllers.py
 Login module controllers.
 """
 import logging
-from flask import Blueprint, render_template, redirect, request, abort, url_for, current_app
+from flask import Blueprint, render_template, redirect, request, abort, url_for
 from flask_login import login_user, LoginManager
 
 from app.mod_login.models import LoginForm
 from app.mod_user import get_user
 
-
 mod_login = Blueprint('login', __name__, url_prefix='')
 
-login_manager = None
+login_manager = LoginManager()
 
 
 def next_is_valid(next):
     return True
 
 
-@mod_login.before_app_first_request
-def init_login_manager():
-    global login_manager
-    login_manager = LoginManager()
-    login_manager.init_app(current_app)
+@mod_login.record_once
+def on_load(state):
+    login_manager.init_app(state.app)
 
-    @login_manager.user_loader
-    def user_loader(email):
-        user = get_user(email)
-        return user
+
+@login_manager.user_loader
+def user_loader(email):
+    user = get_user(email)
+    return user
 
 
 @mod_login.route('/login', methods=['GET', 'POST'])
 def login():
+    form = LoginForm()
     if request.method == 'POST':
-        form = LoginForm()
         if form.validate_on_submit():
             email = request.form.get('email')
             user = get_user(email)
 
             if user.is_authenticated:
-                remember = form.remember.data
-                login_user(user, remember=remember)
+                login_user(user)
 
                 next = request.args.get('next')
 
                 if not next_is_valid(next):
                     return abort(401)
-
-                logging.error("success")
                 return redirect(next or url_for('index.index'))
             return abort(401)
         return abort(400)
-    return render_template('login.html')
+    return render_template('login.html', form=form)
--- app/mod_login/models.py
+++ app/mod_login/models.py
@@ -5,10 +5,10 @@ Login module models.
 """
 
 from flask_wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import PasswordField, StringField
+from wtforms.validators import DataRequired, Email
 
 
 class LoginForm(Form):
-    name = StringField('name', validators=[DataRequired()])
-    name = StringField('password', validators=[DataRequired()])
+    email = StringField('Your Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Your Password', validators=[DataRequired()])
