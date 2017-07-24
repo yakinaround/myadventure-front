9774227405fe66d0ebefb975e9703c3a0b725691
Martin Spier <spiermar@gmail.com>
2017-07-24 00:29:51 -0700

    fix: fixing variable name

--- app/static/js/tracker.js
+++ app/static/js/tracker.js
@@ -108,7 +108,7 @@
 		if (type === 'car' || type === 'tracker') {
 			let d = new Date(Date.parse(timestamp))
 			content = content.concat("<p>{0}</p>".format(d.toString()));
-			content = content.concat("<p>{0}, {1}</p>".format(str(lat), str(lon)));
+			content = content.concat("<p>{0}, {1}</p>".format(str(lat), str(lng)));
 		}
 
 		if (message) {
